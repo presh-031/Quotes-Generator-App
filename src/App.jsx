70999eaa5cf44ea1159ad3e785b601a2250d6bce
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-04 16:04:32 +0100

    remodifying with proper conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,83 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Quote from "./components/Quote";
 import { MdTrendingFlat, MdCached } from "react-icons/md";
+import axios from "axios";
 
 const App = () => {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
-  const [genre, setGenre] = useState("");
-  const [quoteEl, setQuoteEl] = useState("");
+  const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [genre, setGenre] = useState(null);
+  const [quotesEl, setQuotesEl] = useState(null);
 
-  async function getQuotes() {
-    const res = await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random");
-    const data = await res.json();
-
-    const newQuote = data.data[0].quoteText;
-    const newQuoteAuthor = data.data[0].quoteAuthor;
-    const newQuoteGenre = data.data[0].quoteGenre;
-
-    setQuote(newQuote);
-    setAuthor(newQuoteAuthor);
-    setGenre(newQuoteGenre);
-    setQuoteEl("");
-  }
-
-  async function getAllQuotes() {
-    const res = await fetch(`https://quote-garden.herokuapp.com/api/v3/quotes?author=${author}`);
-    const data = await res.json();
-
-    // console.log(data.data);
-    const allQuotes = data.data;
-    setQuoteEl(
-      allQuotes.map((quoteObj) => {
-        return <Quote key={quoteObj._id} quote={quoteObj.quoteText} />;
+  function getQuote() {
+    axios
+      .get("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+      .then((response) => {
+        const data = response.data.data[0];
+        setQuote(data.quoteText);
+        setAuthor(data.quoteAuthor);
+        setGenre(data.quoteGenre);
       })
-    );
-    setQuote("");
+      .catch((error) => {
+        console.log(error);
+      });
+    // setQuotesEl("");
   }
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  function getAllQuotes() {
+    axios
+      .get(`https://quote-garden.herokuapp.com/api/v3/quotes?author=${author}`)
+      .then((response) => {
+        const data = response.data.data;
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  //   const data = await res.json();
+
+  //   // console.log(data.data);
+  //   const allQuotes = data.data;
+  //   setQuotesEl(
+  //     allQuotes.map((quoteObj) => {
+  //       return <Quote key={quoteObj._id} quote={quoteObj.quoteText} />;
+  //     })
+  //   );
+  //   setQuote("");
+  // }
   return (
     <div className="App">
       <header>
-        <div className="random-btn" onClick={getQuotes}>
+        <div className="random-btn" onClick={getQuote}>
           random <MdCached className="random-icon" size="2.2em" />
         </div>
       </header>
       <main>
-        <Quote quote={quote} />
-        {quoteEl}
+        {quote && <Quote quote={quote} />}
+        {/* <div>{quotesEl}</div> */}
 
-        <section className="person" onClick={getAllQuotes}>
-          <div>
-            <p className="name">{author}</p>
-            <p className="about">{genre}</p>
-          </div>
-          <div>
-            <MdTrendingFlat size="2.5em" color="#fff" />
-          </div>
-        </section>
+        {quote && (
+          <section
+            className="person"
+            onClick={() => {
+              setQuote(null);
+              getAllQuotes();
+            }}
+          >
+            <div>
+              <p className="name">{author}</p>
+              <p className="about">{genre}</p>
+            </div>
+            <div>
+              <MdTrendingFlat size="2.5em" color="#fff" />
+            </div>
+          </section>
+        )}
       </main>
     </div>
   );
